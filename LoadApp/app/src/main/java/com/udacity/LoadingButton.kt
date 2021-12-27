0106bbe748bc08538fcd87211387fb2089b17b14
Michal Majernik <majernik.miso@gmail.com>
2021-12-27 23:02:51 +0100

    [TASK] Display download button

--- LoadApp/app/src/main/java/com/udacity/LoadingButton.kt
+++ LoadApp/app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,31 +3,59 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
     private var widthSize = 0
     private var heightSize = 0
 
+    private var textColor = 0
+    private var buttonBackground = 0
+
     private val valueAnimator = ValueAnimator()
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        typeface = Typeface.create( "", Typeface.NORMAL)
+    }
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
     }
 
-
     init {
-
+        isClickable = true
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            buttonBackground = getColor(R.styleable.LoadingButton_buttonBackground, 0)
+            textColor = getColor(R.styleable.LoadingButton_textColor, 0)
+        }
     }
 
 
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        paint.color = buttonBackground
+        canvas.drawRect(0f, 0f, widthSize.toFloat(), heightSize.toFloat(), paint)
+
+        paint.color = textColor
+        canvas.drawText(
+                resources.getString(R.string.download),
+                widthSize.toFloat() / 2,
+                (heightSize.toFloat() - paint.descent() - paint.ascent()) / 2,
+                paint
+        )
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
